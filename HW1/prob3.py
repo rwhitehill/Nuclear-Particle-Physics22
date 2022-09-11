c0330a220fbcb05a10843d30b1c817692f73888b
Richard Whitehill <whiteric@jlab.org>
2022-09-11 14:38:19 -0500

    corrected type

--- HW1/prob3.py
+++ HW1/prob3.py
@@ -4,10 +4,12 @@ from scipy.constants import hbar,c,m_e
 ureg = pint.UnitRegistry()
 Q_   = ureg.Quantity
 
+
 dx   = Q_(1e-13,'cm')
 hbar = Q_(hbar,'J*s')
 c    = Q_(c,'m/s')
 m_e  = Q_(m_e,'kg')
+
 
 p = (hbar/2/dx).to('kg*m/s')
 print('{:~.1E}'.format(p.to('MeV/m*s')))
